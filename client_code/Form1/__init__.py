a1fc50622fa6e72ce9f8d4d47b413d347f2bf859
sundos95alsubhi@gmail.com <sundos95alsubhi@gmail.com>
2024-05-28 20:55:40 +0000

    Edited settings, theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
