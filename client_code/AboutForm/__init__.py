a60b3abf14b3adb69a07141b1a367261de5e2ef2
sundos95alsubhi@gmail.com <sundos95alsubhi@gmail.com>
2024-05-28 21:19:42 +0000

    Created ContactForm, MainPage and AboutForm; Edited settings; Deleted Form1

--- client_code/AboutForm/__init__.py
+++ client_code/AboutForm/__init__.py
@@ -1,14 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import AboutFormTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class AboutForm(AboutFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
---
+++ client_code/MainPage/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import MainPageTemplate
+from anvil import *
+
+
+class MainPage(MainPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('ContactForm')
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('AboutForm')
